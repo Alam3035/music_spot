8fed8b2eb5dcfa278bed3c4b3240cbbc8ada51dd
Husnan Alam <alam@memoriki.com>
2022-08-12 14:26:43 +0800

    db user infor used for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import GradientLayout from "../components/gradientLayout";
 import prisma from "../lib/prisma";
 import {Box, Flex, Text} from "@chakra-ui/layout";
 import {Image} from "@chakra-ui/react";
+import { useMe } from "../lib/hooks";
 
 const Home = ({artists}) => {
+  const { user } = useMe()
+
   return (
-    <GradientLayout roundImage={true} color="red" subtitle="profile" title="Random User" description="some playlist in the user stuff" image='https://tinted-gym-f99.notion.site/image/https:%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png?dl=0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2'>
+    <GradientLayout roundImage={true} color="red" subtitle="profile" title={`${user?.firstName} ${user?.lastName}`} description="some playlist in the user stuff" image='https://tinted-gym-f99.notion.site/image/https:%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png?dl=0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2'>
       <Box color="white" padding="40px">
         <Box marginBottom="40px">
           <Text fontSize="2xl" fontWeight="bold">Top artist this month</Text>
